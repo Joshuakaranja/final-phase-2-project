64cadc80d48d6eb8fa79f407f75e408970ccd774
Jackson <jacksonmungai001@gmail.com>
2025-07-22 12:29:28 +0300

    modified Description.jsx

--- src/Description.jsx
+++ src/Description.jsx
@@ -2,8 +2,11 @@ import React, {useEffect , useState} from 'react'
 
 function Description() {
   return (
-    <div>Description</div>
-  )
+    <div className='description'>
+      <h1>Welcome to Your Travel Journal</h1>
+      <p>Track your dream destinations, add notes, and plan your adventures!</p>
+    </div>
+  );
 }
 
 export default Description
