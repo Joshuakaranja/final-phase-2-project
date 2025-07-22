d3c6544bd995703b7fea6d3515fd362ee3a682a3
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-07-22 13:12:53 +0300

    Setting up edit state and handlers for destination editing

--- src/DestinationList.jsx
+++ src/DestinationList.jsx
@@ -2,6 +2,14 @@ import React, {useEffect, useState} from 'react'
 
 function DestinationList() {
   const [destinations, setDestinations]=useState([])
+  const [editingId, setEditingId] =useState(null);
+  const [formData, setFormData]= useState({
+    destination:"",
+    description:"",
+    date:"",
+    budgetAmount:"",
+    image:""
+  });
   useEffect(()=>{
     fetch("https://json-server-books-2.onrender.com/destinations")
     .then(r=>r.json())
@@ -10,6 +18,23 @@ function DestinationList() {
       setDestinations(data);
     });
   }, [])
+
+  function handleEdit(destination){
+    setEditingId(destination.id);
+    setFormData({
+      destination:destination.destination,
+      description:destination.description,
+      date:destination.date,
+      budgetAmount:destination.budgetAmount,
+      image:destination.image
+    });
+  }
+
+  function handleChange(e){
+    setFormData({
+      ...formData,[e.target.name]: e.target.value
+    });
+  }
   return (
     <div>
       <h2>Destination List</h2>
