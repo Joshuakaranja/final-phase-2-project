37bb728666ff58784847027c3912000b74ca17f4
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-07-22 12:57:56 +0300

    display full destination details using GET fetch method

--- src/DestinationList.jsx
+++ src/DestinationList.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function DestinationList() {
+  const [destinations, setDestinations]=useState([])
+  useEffect(()=>{
+    fetch("https://json-server-books-2.onrender.com/destinations")
+    .then(r=>r.json())
+    .then(data=>{
+      console.log(data);
+      setDestinations(data);
+    });
+  }, [])
   return (
-    <div>DestinationList</div>
+    <div>
+      <h2>Destination List</h2>
+      {destinations.map((destination)=>{
+        return(
+        <div key={destination.id}>
+          <h3>{destination.destination}</h3>
+          <p>{destination.description}</p>
+          <p>{destination.date}</p>
+          <p>{destination.budgetAmount}</p>
+          <img src={destination.image} alt={destination.destination} style={{width:"300px"}} />
+        </div>
+        )
+      })}
+    </div>
   )
 }
 
