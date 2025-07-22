25f70927445e622e57f4d10057ccbef3aafc4d18
Jackson <jacksonmungai001@gmail.com>
2025-07-22 13:30:04 +0300

    greetings update on Description.jsx

--- src/Description.jsx
+++ src/Description.jsx
@@ -1,6 +1,24 @@
-import React, {useEffect , useState} from 'react'
+import React, {useState , useEffect} from 'react';
 
 function Description() {
+  const [greeting, setGreeting] = useState('Welcome');
+
+  useEffect(() => {
+    const updateGreeting = () => {
+    const hour = new Date().getHours();
+    if (hour < 12) {
+      setGreeting('Good Morning');
+    } else if (hour < 18) {
+       setGreeting('Good Afternoon');
+    } else {
+       setGreeting('Good Evening');
+  } 
+ };
+
+    updateGreeting(); 
+  }, []);
+
+
   return (
     <div className='description'>
       <h1>Welcome to Your Travel Journal</h1>
